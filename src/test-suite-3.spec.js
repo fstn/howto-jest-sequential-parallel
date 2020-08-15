1433bc60f5d080c38899bf3a0ffbac731658b14b
stephen.zambaux <stephen.zambaux@gmail.com>
2020-08-15 09:36:19 +0200

    ok now test are sequentials

--- src/test-suite-3.spec.js
+++ src/test-suite-3.spec.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 
 describe("Test suite 3", () => {
   it("Test suite 3 -> Test 1", (done) => {
-    fs.appendFileSync("result.txt", "\Start: Test suite 3 -> Test 1");
+    fs.appendFileSync("result.txt", "\nStart: Test suite 3 -> Test 1");
       setTimeout(() => {
-          fs.appendFileSync("result.txt", "\End: Test suite 3 -> Test 1");
+          fs.appendFileSync("result.txt", "\nEnd: Test suite 3 -> Test 1");
           done();
       }, Math.random() * 2000);
   });
 
   it("Test suite 3 -> Test 2", (done) => {
-    fs.appendFileSync("result.txt", "\Start: Test suite 3 -> Test 2");
+    fs.appendFileSync("result.txt", "\nStart: Test suite 3 -> Test 2");
       setTimeout(() => {
-          fs.appendFileSync("result.txt", "\End: Test suite 3 -> Test 2");
+          fs.appendFileSync("result.txt", "\nEnd: Test suite 3 -> Test 2");
           done();
       }, Math.random() * 2000);
   });
